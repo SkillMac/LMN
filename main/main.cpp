ad55d6a615eb1de6e25b51c89c1160427faa6c97
weidonghao <2807582398@qq.com>
2019-01-02 14:44:48 +0800

    [fix]: 1.fix scons build cache problem 2.fix libcmt.lib conflicts with other libraries 3.fix LINK : 没有找到 HelloWorld.exe 或上一个增量链接没有生成它；正在执行完全链接 [add]: 1.add sub system console

--- main/main.cpp
+++ main/main.cpp
@@ -1,9 +1,11 @@
+#include <iostream>
 #include "window.h"
 
 int main()
 {
 	Dialog* dialog = Dialog::get_singleton();
 	dialog->init();
+	std::cout << "startup success" << std::endl;
 	dialog->setup("LMN", 1280, 720);
 	return 0;
 }
